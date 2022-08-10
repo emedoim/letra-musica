41ee3389672cb55b77f31327e2d4f1a008bacf9a
emedoim <109695655+emedoim@users.noreply.github.com>
2022-08-10 11:19:59 -0300

    Versão 1.0

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('#form')
+const searchInput = document.querySelector('#search')
+const songsContainer = document.querySelector('#songs-container')
+const prevAndNextContainer = document.querySelector('#prev-and-next-container')
+
+
+const apiURL = `https://api.lyrics.ovh`
+
+const fetchSongs = term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`)
+    console.log(response)
+    /*fetch(`${apiURL}/suggest/${term}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)       
+        })*/
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const searchTerm = searchInput.value.trim()
+    if(!searchTerm){
+        songsContainer.innerHTML = `<li class="warning-message"> Por favor, digite um termo válido. </li>`
+        return
+    }
+
+    fetchSongs(searchTerm)
+    
+})
